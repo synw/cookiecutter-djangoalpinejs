b340704987d912b378c6d929376a3fea5f4b21e8
synw <synwx@protonmail.com>
2022-02-23 13:24:07 +0100

    Fix in settings

--- {{cookiecutter.project_name}}/sandbox/settings/base.py
+++ {{cookiecutter.project_name}}/sandbox/settings/base.py
@@ -156,7 +156,7 @@ INSTALLED_APPS = [
     "django_extensions",
     "tailwind",
     "theme",
-    "django_browser_reload"
+    "django_browser_reload",
     "{{ cookiecutter.project_name }}.apps.{{ cookiecutter.project_name }}Config",
 ]
 
